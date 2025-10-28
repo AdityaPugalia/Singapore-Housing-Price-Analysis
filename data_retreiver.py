4ad5b884b901f35f62ea4446f171ddd4b0cadc38
AdityaPugalia <adityapugalia30@gmail.com>
2025-10-29 01:25:37 +0800

    Added rental contract data

--- data_retreiver.py
+++ data_retreiver.py
@@ -10,6 +10,7 @@ load_dotenv()
 
 AUTHENTICATION_URL = 'https://eservice.ura.gov.sg/uraDataService/insertNewToken/v1'
 TRANSACTION_URL = 'https://eservice.ura.gov.sg/uraDataService/invokeUraDS/v1?service=PMI_Resi_Transaction&batch={batch_number}'
+RENTAL_URL = 'https://eservice.ura.gov.sg/uraDataService/invokeUraDS/v1?service=PMI_Resi_Rental&refPeriod={ref_period}'
 access_key = os.getenv('URA_ACCESS_KEY')
 
 def write_to_file(filename, data, col):
@@ -86,10 +87,55 @@ def get_transaction_data(batch_number):
         traceback.print_exc()
         return None
         
+def get_rental_data(ref_period):
+    token = os.getenv('URA_TOKEN')
+    headers = {
+        'AccessKey' : access_key,
+        'Token' : token,
+        "User-Agent": "curl/7.64.1",
+    }
+    try: 
+        response = requests.get(RENTAL_URL.format(ref_period=ref_period), headers=headers)
+        print(response.status_code)
+        if response.status_code != 200:
+            print("Failed to retrieve rental data")
+            return None
+        json_data = response.json().get('Result')
+        print(f"Rental data for reference period {ref_period} fetched successfully.")
+        parse_rental_data(ref_period,json_data)
+        return
+    except requests.RequestException as e:
+        print(headers)
+        print(f"Error fetching rental data: {e}")
+        traceback.print_exc()
+        return None
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        traceback.print_exc()
+        return None
 
+def parse_rental_data(ref_period, json_data):
+    filename = f"Data/property_rentals_{ref_period}.csv"
+    csv_data = []
+    col = set(["project", "street", "x", "y"])
+    for data in json_data:
+        for txn in data["rental"]:
+            row = {
+                "project": data["project"],
+                "street": data["street"],
+                "x": data.get("x", ""),
+                "y": data.get("y", ""),
+                **txn   # unpack transaction fields
+            }
+            col.update(row.keys())
+            csv_data.append(row)
+
+    write_to_file(filename, csv_data, list(col))
+    print(f"Data for rental contracts written to {filename}")
+    
 if __name__ == '__main__':
-    pass
     # get_access_token()
     # for batch in range(1, 5):
     #     get_transaction_data(batch)
-    
+    for ref_period in ['20Q3', '20Q4', '21Q1', '21Q2', '21Q3', '21Q4', '22Q1', '22Q2', '22Q3', '22Q4', '23Q1', '23Q2', '23Q3', '23Q4', '24Q1', '24Q2', '24Q3', '24Q4', '25Q1', '25Q2', '25Q3']:
+        get_rental_data(ref_period)
